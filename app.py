d9da62de5d5d873f38dbdc2dce08ad5cf9e897fb
Rahul Babu K <rahulbabu101@gmail.com>
2020-05-19 08:24:51 +0530

    update

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 from flask import Flask,render_template,url_for,request,make_response
-import pandas as pd 
-import json
-from flask_cors import cross_origin
-import string
-import re
-import pandas as pd
-import numpy as np
-
-
 
 app = Flask(__name__)
 
